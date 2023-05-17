9e470d3312bf4df44bdf500d5186855dcf702c35
NAHO <90870942+trueNAHO@users.noreply.github.com>
2023-05-17 15:13:57 +0200

    feat(rpg): start from scratch with a proper game loop

--- src/main/java/rpg/RPG.java
+++ src/main/java/rpg/RPG.java
@@ -1,86 +1,77 @@
 package rpg;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.event.EventHandler;
-import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.image.*;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class RPG extends Application {
+  private static final Color BACKGROUND_COLOR = Color.BLUE;
+  private static final Color PLAYER_COLOR = Color.RED;
+  private static final double FPS = 24;
+  private static final double PLAYER_HEIGHT = 50;
+  private static final double PLAYER_WIDTH = 50;
+  private static final double PLAYER_X = 100;
+  private static final double PLAYER_Y = 100;
+  private static final int SCENE_HEIGHT = 800;
+  private static final int SCENE_WIDTH = 800;
+
+  private static final double MS_PER_UPDATE = 1000 / FPS;
+
+  private Rectangle player;
+
+  private Group root = new Group();
+  private Scene scene = new Scene(this.root, SCENE_WIDTH, SCENE_HEIGHT, BACKGROUND_COLOR);
+  private double lag = 0;
+  private long previousTime = 0;
+
   public static void main(String[] args) {
     launch(args);
   }
 
-  private static final int size = 10;
-  private int x = 5;
-  private int y = 5;
-  private Stage stage;
-
-  private void createScene() {
-    Image grassImage = new Image("sprites/grass-tile.png");
-    Image bulbasaur = new Image("sprites/bulbasaur.png");
-    StackPane[][] sprites = new StackPane[size][size];
-    TilePane tiles = new TilePane();
-    tiles.setPrefColumns(size);
-    tiles.setPrefRows(size);
-    tiles.setTileAlignment(Pos.CENTER);
-
-    for (int i = 0; i < size; ++i) {
-      for (int j = 0; j < size; ++j) {
-        sprites[i][j] = new StackPane();
-        sprites[i][j].getChildren().add(makeView(grassImage));
-        tiles.getChildren().add(sprites[i][j]);
-      }
-    }
-
-    System.out.println("Set bulbasaur to " + x + "," + y);
-    sprites[x][y].getChildren().add(makeView(bulbasaur));
-
-    Scene scene = new Scene(tiles);
-    scene.setOnKeyPressed(
-        new EventHandler<KeyEvent>() {
-          @Override
-          public void handle(KeyEvent event) {
-            sprites[x][y].getChildren().remove(1);
-            switch (event.getCode()) {
-              case UP:
-                if (x > 0) --x;
-                break;
-              case DOWN:
-                if (x < size - 1) ++x;
-                break;
-              case LEFT:
-                if (y > 0) --y;
-                break;
-              case RIGHT:
-                if (y < size - 1) ++y;
-                break;
-              default:
-                break;
-            }
-            System.out.println("Set bulbasaur to " + x + "," + y);
-            sprites[x][y].getChildren().add(makeView(bulbasaur));
-          }
-        });
-    stage.setScene(scene);
-  }
-
   @Override
-  public void start(Stage stage) {
-    this.stage = stage;
-    createScene();
-    stage.setWidth(640);
-    stage.setHeight(640);
-    stage.sizeToScene();
-    stage.show();
+  public void start(Stage primaryStage) {
+    this.player = createPlayer();
+    this.root.getChildren().add(this.player);
+
+    primaryStage.setTitle("RPG");
+    primaryStage.setScene(this.scene);
+    primaryStage.show();
+
+    AnimationTimer gameLoop =
+        new AnimationTimer() {
+          @Override
+          public void handle(long currentTime) {
+            double elapsedMilliseconds = (currentTime - previousTime) / 1e6;
+            previousTime = currentTime;
+            lag += elapsedMilliseconds;
+
+            processInput(elapsedMilliseconds);
+
+            while (lag >= MS_PER_UPDATE) {
+              update(elapsedMilliseconds);
+              lag -= MS_PER_UPDATE;
+            }
+
+            render(elapsedMilliseconds);
+          }
+        };
+
+    gameLoop.start();
   }
 
-  private ImageView makeView(Image image) {
-    ImageView view = new ImageView();
-    view.setImage(image);
-    return view;
+  private Rectangle createPlayer() {
+    Rectangle rectangle = new Rectangle(PLAYER_X, PLAYER_Y, PLAYER_WIDTH, PLAYER_HEIGHT);
+    rectangle.setFill(PLAYER_COLOR);
+    return rectangle;
   }
+
+  private void processInput(double elapsedMilliseconds) {}
+
+  private void render(double elapsedMilliseconds) {}
+
+  private void update(double elapsedMilliseconds) {}
 }
