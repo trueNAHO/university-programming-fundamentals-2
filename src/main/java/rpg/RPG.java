736d344ced58b0c04d3645c559d8938f6aeb4407
NAHO <90870942+trueNAHO@users.noreply.github.com>
2023-05-26 01:13:55 +0200

    Revert "Added house collision so player cant walk through graphic"
    This reverts commit d86852017c7f56f62140563ea062856dc69cc8ad.
    
    "The Perils of Rebasing":
    https://git-scm.com/book/en/v2/Git-Branching-Rebasing#_rebase_peril

--- src/main/java/rpg/RPG.java
+++ src/main/java/rpg/RPG.java
@@ -18,6 +18,8 @@ import rpg.command.PlayerMoveDownCommand;
 import rpg.command.PlayerMoveLeftCommand;
 import rpg.command.PlayerMoveRightCommand;
 import rpg.command.PlayerMoveUpCommand;
+import rpg.command.fieldAddCommand;
+import rpg.command.fieldGrowAllFieldCommand;
 import rpg.day_night_cycle.DayNightCycle;
 import rpg.entities.player.Player;
 import rpg.entities.player.states.IdleState;
@@ -25,247 +27,146 @@ import rpg.field.Field;
 import rpg.input.InputHandler;
 
 public class RPG extends Application {
-    private static final double FPS = 24;
-    private static final double PLAYER_HEIGHT = 66;
-    private static final double PLAYER_WIDTH = 38;
-    private static final double PLAYER_X = 100;
-    private static final double PLAYER_Y = 100;
-    private static final double HOUSE_X = 200;
-    private static final double HOUSE_Y = 200;
-    private static final int SCENE_HEIGHT = 900;
-    private static final int SCENE_WIDTH = 1500;
-    private static final Color BACKGROUND_COLOR = Color.BLUE;
-    private static final Image BACKGROUND_IMAGE = new Image("sprites/background.png", SCENE_WIDTH, SCENE_HEIGHT, false,
-            false);
-    private static final Color PLAYER_COLOR = Color.RED;
-    private static final Image PLAYER_IMAGE = new Image("sprites/GraveRobber.png");
-    private static final Image HOUSE_IMAGE = new Image("sprites/house-002.png");
+  private static final double FPS = 24;
+  private static final double PLAYER_HEIGHT = 66;
+  private static final double PLAYER_WIDTH = 38;
+  private static final double PLAYER_X = 100;
+  private static final double PLAYER_Y = 100;
+  private static final int SCENE_HEIGHT = 900;
+  private static final int SCENE_WIDTH = 1500;
+  private static final Color BACKGROUND_COLOR = Color.BLUE;
+  private static final Image BACKGROUND_IMAGE =
+      new Image("sprites/background.png", SCENE_WIDTH, SCENE_HEIGHT, false, false);
+  private static final Color PLAYER_COLOR = Color.RED;
+  private static final Image PLAYER_IMAGE = new Image("sprites/GraveRobber.png");
+  private static final Image HOUSE_IMAGE = new Image("sprites/house-002.png");
+  private static final Image PLANT_IMAGE = new Image("sprites/TEST.png");
+  private static final Image FIEL_IMAGE = new Image("sprites/field.png");
 
-    private static final double MS_PER_UPDATE = 1000 / FPS;
+  private static final double MS_PER_UPDATE = 1000 / FPS;
 
-    private ArrayList<Block> blocks = new ArrayList<>();
-    private Block house = new Block(HOUSE_X, HOUSE_Y, 200, 200, HOUSE_IMAGE);
-    // private DayNightCycle dayNightCycle = new DayNightCycle(true);
-    private Field field = new Field(300, 500, 500, 250, Color.BROWN, Color.GREEN, 5, 8, 20);
-    private Group root = new Group();
-    private InputHandler inputHandler = new InputHandler();
-    private Player player = new Player(PLAYER_X, PLAYER_Y, PLAYER_WIDTH, PLAYER_HEIGHT, PLAYER_IMAGE);
-    private Scene scene = new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
-    private double elapsedMilliseconds = 0;
-    private double lag = 0;
-    private long previousTime = 0;
+  private ArrayList<Block> blocks = new ArrayList<>();
+  private Block house = new Block(200, 200, 200, 200, HOUSE_IMAGE);
+  private Field field = new Field(300, 500, 500, 250, FIEL_IMAGE, PLANT_IMAGE, 5, 8, 20);
+  private DayNightCycle dayNightCycle = new DayNightCycle(true, this.field);
+  private Group root = new Group();
+  private InputHandler inputHandler = new InputHandler();
+  private Player player = new Player(PLAYER_X, PLAYER_Y, PLAYER_WIDTH, PLAYER_HEIGHT, PLAYER_IMAGE);
+  private Scene scene = new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
+  private double elapsedMilliseconds = 0;
+  private double lag = 0;
+  private long previousTime = 0;
 
-    private Command playerMoveDownCommand = new PlayerMoveDownCommand(player);
-    private Command playerMoveLeftCommand = new PlayerMoveLeftCommand(player);
-    private Command playerMoveRightCommand = new PlayerMoveRightCommand(player);
-    private Command playerMoveUpCommand = new PlayerMoveUpCommand(player);
+  private Command playerMoveDownCommand = new PlayerMoveDownCommand(player);
+  private Command playerMoveLeftCommand = new PlayerMoveLeftCommand(player);
+  private Command playerMoveRightCommand = new PlayerMoveRightCommand(player);
+  private Command playerMoveUpCommand = new PlayerMoveUpCommand(player);
+  private Command fieldGrowAllFieldCommand = new fieldGrowAllFieldCommand(field);
+  private Command fieldAddCommand = new fieldAddCommand(field);
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 
-    @Override
-    public void start(Stage primaryStage) {
+  @Override
+  public void start(Stage primaryStage) {
 
-        root.getChildren().add(new ImageView(BACKGROUND_IMAGE));
-        setupDefaultKeyBindings();
-        setupBlocks();
-        setupPlayer();
+    root.getChildren().add(new ImageView(BACKGROUND_IMAGE));
+    setupDefaultKeyBindings();
+    setupBlocks();
+    setupPlayer();
 
-        primaryStage.setTitle("RPG");
-        primaryStage.setScene(this.scene);
+    primaryStage.setTitle("RPG");
+    primaryStage.setScene(this.scene);
 
-        handleInput();
+    handleInput();
 
-        primaryStage.show();
+    primaryStage.show();
 
-        AnimationTimer gameLoop = new AnimationTimer() {
-            @Override
-            public void handle(long currentTime) {
-                elapsedMilliseconds = (currentTime - previousTime) / 1e6;
-                previousTime = currentTime;
-                lag += elapsedMilliseconds;
+    AnimationTimer gameLoop =
+        new AnimationTimer() {
+          @Override
+          public void handle(long currentTime) {
+            elapsedMilliseconds = (currentTime - previousTime) / 1e6;
+            previousTime = currentTime;
+            lag += elapsedMilliseconds;
 
-                while (lag >= MS_PER_UPDATE) {
-                    update(MS_PER_UPDATE);
-                    lag -= MS_PER_UPDATE;
-                }
-
-                render(elapsedMilliseconds);
+            while (lag >= MS_PER_UPDATE) {
+              update(MS_PER_UPDATE);
+              lag -= MS_PER_UPDATE;
             }
+
+            render(elapsedMilliseconds);
+          }
         };
 
-        gameLoop.start();
+    gameLoop.start();
+  }
+
+  private void handleInput() {
+    this.scene.setOnKeyPressed(
+        event -> {
+          Command command = this.inputHandler.handleInput(event.getCode());
+          command.execute(this.elapsedMilliseconds);
+        });
+
+    this.scene.setOnKeyReleased(
+        event -> {
+          Command command = this.inputHandler.handleInput(event.getCode());
+
+          if (command == this.playerMoveDownCommand
+              || command == this.playerMoveLeftCommand
+              || command == this.playerMoveRightCommand
+              || command == this.playerMoveUpCommand) {
+            this.player.setState(new IdleState());
+          }
+        });
+  }
+
+  private void render(double elapsedMilliseconds) {}
+
+  private void setupBlocks() {
+    this.blocks.add(this.house);
+    this.blocks.add(this.field.field);
+
+    for (int i = 0; i < this.field.plants.size(); i++) {
+      for (int j = 0; j < this.field.plants.get(0).size(); j++) {
+        this.blocks.add(this.field.plants.get(i).get(j));
+      }
     }
 
-    private void handleInput() {
-        this.scene.setOnKeyPressed(
-                event -> {
-                    Command command = this.inputHandler.handleInput(event.getCode());
-                    command.execute(this.elapsedMilliseconds);
-                });
-
-        this.scene.setOnKeyReleased(
-                event -> {
-                    Command command = this.inputHandler.handleInput(event.getCode());
-
-                    if (command == this.playerMoveDownCommand
-                            || command == this.playerMoveLeftCommand
-                            || command == this.playerMoveRightCommand
-                            || command == this.playerMoveUpCommand) {
-                        this.player.setState(new IdleState());
-                    }
-                });
+    for (Block block : this.blocks) {
+      this.root.getChildren().add(block);
     }
+  }
 
-    private void render(double elapsedMilliseconds) {
+  private void setupDefaultKeyBindings() {
+    this.inputHandler.mapInput(KeyCode.DOWN, this.playerMoveDownCommand);
+    this.inputHandler.mapInput(KeyCode.LEFT, this.playerMoveLeftCommand);
+    this.inputHandler.mapInput(KeyCode.RIGHT, this.playerMoveRightCommand);
+    this.inputHandler.mapInput(KeyCode.UP, this.playerMoveUpCommand);
+    this.inputHandler.mapInput(KeyCode.G, this.fieldGrowAllFieldCommand);
+    this.inputHandler.mapInput(KeyCode.A, this.fieldAddCommand);
+  }
+
+  private void setupPlayer() {
+    this.root.getChildren().add(this.player);
+  }
+
+  private void playerCollideBlocks() {
+    for (Block block : this.blocks) {
+      if (player.intersects(block.getBoundsInLocal())) {
+        block.setState(new BlockInteractableState());
+      } else if (block.isInteractable()) {
+        block.setState(new BlockIdleState());
+      }
     }
+  }
 
-    private void setupBlocks() {
-        this.blocks.add(this.house);
-        this.blocks.add(this.field.field);
-
-        for (int i = 0; i < this.field.plants.size(); i++) {
-            for (int j = 0; j < this.field.plants.get(0).size(); j++) {
-                this.blocks.add(this.field.plants.get(i).get(j));
-            }
-        }
-
-        for (Block block : this.blocks) {
-            this.root.getChildren().add(block);
-        }
-    }
-
-    private void setupDefaultKeyBindings() {
-        this.inputHandler.mapInput(KeyCode.DOWN, this.playerMoveDownCommand);
-        this.inputHandler.mapInput(KeyCode.LEFT, this.playerMoveLeftCommand);
-        this.inputHandler.mapInput(KeyCode.RIGHT, this.playerMoveRightCommand);
-        this.inputHandler.mapInput(KeyCode.UP, this.playerMoveUpCommand);
-    }
-
-    private void setupPlayer() {
-        this.root.getChildren().add(this.player);
-    }
-
-    // private void borderControl(Block block) {
-    //   double playerPosX = player.getX();
-    //   double playerPosY = player.getY();
-    //   double houseMinX = block.getBoundsInLocal().getMinX();
-    //   double houseMinY = block.getBoundsInLocal().getMinY();
-    //   double houseMaxX = block.getBoundsInLocal().getMaxX();
-    //   double houseMaxY = block.getBoundsInLocal().getMaxY();
-
-    //   while (player.intersects(
-    //                         block.getBoundsInLocal())) { // Works because of added 10 to playerPosX somehow the
-    //                     // player comes from the left and gets stuck behind the
-    //                     // max of the house even though he should already be reset
-    //                     // when he hits 400.
-    //                     if (playerPosX + 20 >= houseMaxX) {
-    //                         System.out.println("Player X " + playerPosX);
-    //                         System.out.println("Player Y " + playerPosY);
-    //                         System.out.println("You just hit House Max X: " + block.getBoundsInLocal().getMaxX());
-    //                         player.setX(houseMaxX + 1 * MS_PER_UPDATE);
-    //                         player.setState(new IdleState());
-    //                         break;
-    //                     } else if (playerPosY + 50 >= houseMaxY) { // Same as above
-    //                         System.out.println("Player X " + playerPosX);
-    //                         System.out.println("Player Y " + playerPosY);
-    //                         System.out.println("You just hit House MaxY: " + block.getBoundsInLocal().getMaxY());
-    //                         player.setY(houseMaxY + 1 * MS_PER_UPDATE);
-    //                         player.setState(new IdleState());
-    //                         break;
-    //                     } else if (playerPosX <= houseMinX) { // Works, Player comes from above gets blocked by house
-    //                         System.out.println("Player X " + playerPosX);
-    //                         System.out.println("Player Y " + playerPosY);
-    //                         System.out.println("You just hit House Min X: " + block.getBoundsInLocal().getMinX());
-    //                         player.setX(houseMinX - 1 * MS_PER_UPDATE);
-    //                         player.setState(new IdleState());
-    //                         break;
-    //                     } else if (playerPosY <= houseMinY) { // Works, Player comes from left gets blocked by house
-    //                         System.out.println("Player X " + playerPosX);
-    //                         System.out.println("Player Y " + playerPosY);
-    //                         System.out.println("You just hit House MinY: " + block.getBoundsInLocal().getMinY());
-    //                         System.out.println("Bounds " + block.getBoundsInLocal());
-    //                         player.setY(houseMinY - 1 * MS_PER_UPDATE);
-    //                         player.setState(new IdleState());
-    //                         break;
-    //                     } else {
-    //                         System.out.println("Player X " + playerPosX);
-    //                         System.out.println("Player Y " + playerPosY);
-    //                         System.out.println("You just hit House Max X: " + block.getBoundsInLocal().getMaxX());
-    //                         System.out.println("You just hit House MaxY: " + block.getBoundsInLocal().getMaxY());
-    //                         System.out.println("Bounds " + block.getBoundsInLocal());
-    //                         System.out.println("This is the fucking error!");
-    //                     }
-    //                 }
-    // }
-
-    private void playerCollideBlocks() {
-        double playerPosX = player.getX();
-        double playerPosY = player.getY();
-        double houseMinX = house.getBoundsInLocal().getMinX();
-        double houseMinY = house.getBoundsInLocal().getMinY();
-        double houseMaxX = house.getBoundsInLocal().getMaxX();
-        double houseMaxY = house.getBoundsInLocal().getMaxY();
-
-        for (Block block : this.blocks) {
-            if (player.intersects(block.getBoundsInLocal())) {
-                if (block.equals(house)) {
-                    while (player.intersects(
-                            house.getBoundsInLocal())) { // Works because of added 10 to playerPosX somehow the
-                        // player comes from the left and gets stuck behind the
-                        // max of the house even though he should already be reset
-                        // when he hits 400.
-                        if (playerPosX + 20 >= houseMaxX) {
-                            System.out.println("Player X " + playerPosX);
-                            System.out.println("Player Y " + playerPosY);
-                            System.out.println("You just hit House Max X: " + house.getBoundsInLocal().getMaxX());
-                            player.setX(houseMaxX + 1 * MS_PER_UPDATE);
-                            player.setState(new IdleState());
-                            break;
-                        } else if (playerPosY + 50 >= houseMaxY) { // Same as above
-                            System.out.println("Player X " + playerPosX);
-                            System.out.println("Player Y " + playerPosY);
-                            System.out.println("You just hit House MaxY: " + house.getBoundsInLocal().getMaxY());
-                            player.setY(houseMaxY + 1 * MS_PER_UPDATE);
-                            player.setState(new IdleState());
-                            break;
-                        } else if (playerPosX <= houseMinX) { // Works, Player comes from above gets blocked by house
-                            System.out.println("Player X " + playerPosX);
-                            System.out.println("Player Y " + playerPosY);
-                            System.out.println("You just hit House Min X: " + house.getBoundsInLocal().getMinX());
-                            player.setX(houseMinX - 1 * MS_PER_UPDATE);
-                            player.setState(new IdleState());
-                            break;
-                        } else if (playerPosY <= houseMinY) { // Works, Player comes from left gets blocked by house
-                            System.out.println("Player X " + playerPosX);
-                            System.out.println("Player Y " + playerPosY);
-                            System.out.println("You just hit House MinY: " + house.getBoundsInLocal().getMinY());
-                            System.out.println("Bounds " + house.getBoundsInLocal());
-                            player.setY(houseMinY - 1 * MS_PER_UPDATE);
-                            player.setState(new IdleState());
-                            break;
-                        } else {
-                            System.out.println("Player X " + playerPosX);
-                            System.out.println("Player Y " + playerPosY);
-                            System.out.println("You just hit House Max X: " + house.getBoundsInLocal().getMaxX());
-                            System.out.println("You just hit House MaxY: " + house.getBoundsInLocal().getMaxY());
-                            System.out.println("Bounds " + house.getBoundsInLocal());
-                            System.out.println("This is the fucking error!");
-                        }
-                    }
-                }
-                block.setState(new BlockInteractableState());
-            } else if (block.isInteractable()) {
-                block.setState(new BlockIdleState());
-            }
-        }
-    }
-
-    private void update(double elapsedMilliseconds) {
-        // this.dayNightCycle.update(elapsedMilliseconds);
-        this.player.update(elapsedMilliseconds);
-        this.playerCollideBlocks();
-        this.field.update(elapsedMilliseconds);
-    }
+  private void update(double elapsedMilliseconds) {
+    this.dayNightCycle.update(elapsedMilliseconds);
+    this.player.update(elapsedMilliseconds);
+    this.playerCollideBlocks();
+    this.field.update(elapsedMilliseconds);
+  }
 }
