38a15566fe8d56f1539c42175f9befb3fdec31e2
Jean Humbert <hean.humbert@gmail.com>
2023-05-29 19:38:22 +0200

    feat(Player): added javadoc for player class

--- src/main/java/rpg/entities/player/Player.java
+++ src/main/java/rpg/entities/player/Player.java
@@ -7,10 +7,23 @@ import javafx.scene.shape.Rectangle;
 import rpg.entities.player.states.NullState;
 import rpg.entities.player.states.PlayerState;
 
+/**
+ * Represents the player in the game.
+ */
+
 public class Player extends Rectangle {
   private static final double SPEED = 1;
 
   private PlayerState state;
+
+  /**
+   * Constructs a Player object with specified position, size, and color.
+   * @param x The x-coordinate of the player's position.
+   * @param y The y-coordinate of the player's position.
+   * @param width The width of the player.
+   * @param height The height of the player.
+   * @param color The color of the player.
+  */
 
   public Player(double x, double y, double width, double height, Color color) {
     super(x, y, width, height);
@@ -18,27 +31,61 @@ public class Player extends Rectangle {
     setFill(color);
   }
 
+  /**
+   * Constructs a Player object with specified position, size, and color.
+   * @param x The x-coordinate of the player's position.
+   * @param y The y-coordinate of the player's position.
+   * @param width The width of the player.
+   * @param height The height of the player.
+   * @param image The image of the player.
+  */
+
   public Player(double x, double y, double width, double height, Image image) {
     super(x, y, width, height);
     this.state = new NullState();
     setFill(new ImagePattern(image));
   }
 
+  /**
+   * Moves the player down based on deltatime.
+   * @param deltatime The time since the last update.
+   */
+  
   public void moveDown(double deltaTime) {
     setY(getY() + SPEED * deltaTime);
   }
+
+  /**
+   * Moves the player left based on deltatime.
+   * @param deltatime The time since the last update.
+   */
 
   public void moveLeft(double deltaTime) {
     setX(getX() - SPEED * deltaTime);
   }
 
+  /**
+   * Moves the player right based on deltatime.
+   * @param deltatime The time since the last update.
+   */
+
   public void moveRight(double deltaTime) {
     setX(getX() + SPEED * deltaTime);
   }
 
+  /**
+   * Moves the player up based on deltatime.
+   * @param deltatime The time since the last update.
+   */
+
   public void moveUp(double deltaTime) {
     setY(getY() - SPEED * deltaTime);
   }
+
+  /**
+   * Sets state of the player.
+   * @param newState The new state of the player.
+   */
 
   public void setState(PlayerState newState) {
     this.state.exit(this);
@@ -46,6 +93,11 @@ public class Player extends Rectangle {
     this.state.enter(this);
   }
 
+  /**
+   * Updates the player based on deltatime.
+   * @param deltatime The time since the last update.
+   */
+  
   public void update(double deltaTime) {
     this.state.update(this, deltaTime);
   }
