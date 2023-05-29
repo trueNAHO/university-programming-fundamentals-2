954c50df061024f99c3a0d07b1aa1dc28dd40f14
Jean Humbert <hean.humbert@gmail.com>
2023-05-29 19:50:53 +0200

    feat(Field): Added javadoc comments for the field class

--- src/main/java/rpg/field/Field.java
+++ src/main/java/rpg/field/Field.java
@@ -8,6 +8,10 @@ import rpg.blocks.BlockNullState;
 import rpg.inventory.Inventory;
 import rpg.plants.Plant;
 
+/**
+ * Represents a field in the game where plants can be grown.
+*/
+
 public class Field {
 
   public Block field;
@@ -16,6 +20,19 @@ public class Field {
   private double plantSpacing;
   private int columns;
 
+  /**
+   * Constructs a Field object with the specified parameters.
+   * @param x The x-coordinate of the field's position.
+   * @param y The y-coordinate of the field's position.
+   * @param width The width of the field.
+   * @param height The height of the field.
+   * @param fieldImage The image representing the field.
+   * @param plantImage The image representing the plants.
+   * @param rows The number of rows in the field.
+   * @param columns The number of columns in the field.
+   * @param plantSpacing The spacing between plants.
+*/
+  
   public Field(
       double x,
       double y,
@@ -49,16 +66,34 @@ public class Field {
     }
   }
 
+  /**
+   * Removes a plant from the field.
+   * @param block The plant to be removed. 
+   */
+
   public void remove(Plant block) {
     block.setState(new BlockNullState());
     block.setType("empty");
   }
 
+  /**
+   * Sets the type of a plant at the specified position in the field.
+   * @param x The x-coordinate of the position.
+   * @param y The y-coordinate of the position.
+   * @param type The type of the plant.
+   */
+  
   public void set(int x, int y, String type) {
     this.plants.get(x).get(y).setStage(1);
     this.plants.get(x).get(y).setType(type);
   }
 
+  /**
+   * Harvests a plant if it is fully grown and adds it into the inventory.
+   * @param block The plant to be harvested.
+   * @param inventory The inventory to add the harvested plant to.
+   */
+  
   public void harvest(Plant block, Inventory inventory) {
     if (block.fullyGrown()) {
       inventory.add(block.type);
@@ -66,6 +101,12 @@ public class Field {
     }
   }
 
+  /**
+   * Plants a new plant to the field, using the seed from the inventory.
+   * @param block The plant that will be planted.
+   * @param inventory The inventory containing the seed.
+   */
+  
   public void plant(Plant block, Inventory inventory) {
     String item = inventory.getItem();
     if (!item.equals("empty") && item.contains("seed")) {
@@ -75,14 +116,30 @@ public class Field {
     }
   }
 
+  /**
+   * Grows the plant at the specific location in the field.
+   * @param x The x-coordinate of the position.
+   * @param y The y-coordinate of the position.
+   */
+  
   public void grow(int x, int y) {
     this.plants.get(x).get(y).grow();
   }
+
+  /**
+   * Resets the plant at the specific location in the field.
+   * @param x The x-coordinate of the position.
+   * @param y The y-coordinate of the position.
+   */
 
   public void reset(int x, int y) {
     this.plants.get(x).get(y).reset();
   }
 
+  /**
+   * Grows all the plants in the field.
+   */
+  
   public void growAllField() {
     for (int i = 0; i < rows; i++) {
       for (int j = 0; j < columns; j++) {
@@ -91,6 +148,10 @@ public class Field {
     }
   }
 
+  /**
+   * Resets all the plants in the field.
+   */
+  
   public void resetAllField() {
     for (int i = 0; i < rows; i++) {
       for (int j = 0; j < columns; j++) {
@@ -99,6 +160,11 @@ public class Field {
     }
   }
 
+  /**
+   * Updates the field and all plants in the field based on the deltaTime.
+   * @param deltaTime The time elapsed since the last update.
+   */
+  
   public void update(double deltaTime) {
     this.field.update(deltaTime);
 
