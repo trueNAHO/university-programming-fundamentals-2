251b3da06a5ddb72f4afb9d5493e46a9cd3154a5
Jean Humbert <hean.humbert@gmail.com>
2023-05-29 20:17:07 +0200

    feat(slot): added the javadco for the slot class

--- src/main/java/rpg/inventory/Slot.java
+++ src/main/java/rpg/inventory/Slot.java
@@ -3,15 +3,33 @@ package rpg.inventory;
 import javafx.scene.image.Image;
 import rpg.blocks.Block;
 
+/**
+ * Represents a slot block.
+ */
+
 public class Slot extends Block {
 
+  /**
+   * Constructs a Slot object with the specified coordinates.
+   * @param x The x-coordinate of the slot.
+   * @param y The y-coordinate of the slot.
+   */
+  
   public Slot(double x, double y) {
     super(x, y, 53, 53, new Image("sprites/slot/slot_stage_1.png"));
   }
 
+  /**
+   * Selects the slot by changing its image to the selected stage.
+   */
+  
   public void select() {
     changeImage(new Image("sprites/slot/slot_stage_2.png"));
   }
+
+  /**
+   * Unselects the slot by changing its image to the unselected stage.
+   */
 
   public void unselect() {
     changeImage(new Image("sprites/slot/slot_stage_1.png"));
