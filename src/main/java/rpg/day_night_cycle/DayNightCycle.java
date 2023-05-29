5b0510bad4f4743d709f68fb38a61294337fa05d
Jean Humbert <hean.humbert@gmail.com>
2023-05-29 19:31:17 +0200

    feat(D&NCycle): Added Javadoc comments for day and night cycle

--- src/main/java/rpg/day_night_cycle/DayNightCycle.java
+++ src/main/java/rpg/day_night_cycle/DayNightCycle.java
@@ -2,6 +2,10 @@ package rpg.day_night_cycle;
 
 import rpg.field.Field;
 
+/**
+ * Represents the day and night cycle of the game
+ */
+
 public class DayNightCycle {
   private static final double DAY_DURATION_MS = 2 * 1000;
   private static final double NIGHT_DURATION_MS = 2 * 1000;
@@ -11,6 +15,12 @@ public class DayNightCycle {
   public double nightDuration;
   private Field field;
 
+  /**
+   * Constructs a DayNightCycle object
+   * @param isDay Indicates if it's currently day
+   * @param field The field block of the game
+   */
+  
   public DayNightCycle(boolean isDay, Field field) {
     this.dayDuration = 0;
     this.isDay = isDay;
@@ -18,6 +28,11 @@ public class DayNightCycle {
     this.field = field;
   }
 
+  /**
+   * Transitions from night to day
+   * @param deltatime The time elapsed since last update
+   */
+  
   public void transitionToDay(double deltaTime) {
     this.isDay = true;
     this.nightDuration = 0;
@@ -25,12 +40,22 @@ public class DayNightCycle {
     this.field.growAllField();
   }
 
+  /**
+   * Transitions from day to night
+   * @param deltatime The time elapsed since last update
+   */
+
   public void transitionToNight(double deltaTime) {
     this.isDay = false;
     this.dayDuration = 0;
     this.nightDuration = deltaTime;
   }
 
+  /**
+   * Updates the day and night cycle
+   * @param deltatime The time elapsed since last updates
+   */
+  
   public void update(double deltaTime) {
     if (this.isDay) {
       double newDayDuration = this.dayDuration + deltaTime;
