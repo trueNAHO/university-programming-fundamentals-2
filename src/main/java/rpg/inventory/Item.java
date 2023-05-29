bc7f3b7d2a2ec2d1e484780ef9fdd3475e8a5ee2
Jean Humbert <hean.humbert@gmail.com>
2023-05-29 20:31:01 +0200

    feat(item): Added javadoc comments for the item class.

--- src/main/java/rpg/inventory/Item.java
+++ src/main/java/rpg/inventory/Item.java
@@ -3,29 +3,56 @@ package rpg.inventory;
 import javafx.scene.image.Image;
 import rpg.blocks.Block;
 
+/**
+ * Represents an item block.
+ */
+
 public class Item extends Block {
   public String type;
   public int amount = 0;
   private int maxAmount = 64;
-  // new Image("sprites/TEST.png", width, height, false, false)
 
+  /**
+   * Constructs an Item object with the specified coordinates, type, width, and height.
+   * @param x The x-coordinate of the item.
+   * @param y The y-coordinate of the item.
+   * @param type The type of the item.
+   * @param width The width of the item.
+   * @param height The height of the item.
+   */
+  
   public Item(double x, double y, String type, double width, double height) {
     super(x, y, width, height, new Image("sprites/items/" + type + ".png"));
     this.type = type;
     this.amount = 0;
   }
 
+  /**
+   * Adds the specified amount to the item.
+   * @param x The amount to add.
+  */
+   */
+  
   public void add(int x) {
     if (inTheLimit(x)) {
       this.amount += x;
     }
   }
 
+  /**
+   * Adds one unit to the item.
+  */
+  
   public void add() {
     if (inTheLimit()) {
       this.amount++;
     }
   }
+
+  /**
+   * Removes the specified amount from the item.
+   * @param x The amount to remove.
+  */
 
   public void remove(int x) {
     if (inTheLimit(x)) {
@@ -36,6 +63,10 @@ public class Item extends Block {
     }
   }
 
+  /**
+   * Removes one unit from the item.
+  */
+
   public void remove() {
     if (inTheLimit()) {
       this.amount--;
@@ -45,6 +76,11 @@ public class Item extends Block {
     }
   }
 
+  /**
+   * Checks if the item is within the limit, between 0 and the max amount (64).
+   * @return True if the item is within the limit and false otherwise.
+   */
+  
   public boolean inTheLimit() {
     if (this.amount < maxAmount && this.amount > 0) {
       return true;
@@ -53,6 +89,12 @@ public class Item extends Block {
     }
   }
 
+  /**
+   * Checks if the item is within the limit, 
+   * (current amount + x is less than or equal to maxAmount and current amount is greater than or equal to x).
+   * @return True if the item is within the limit and false otherwise.
+   */
+  
   public boolean inTheLimit(int x) {
     if (this.amount + x <= maxAmount && this.amount >= x) {
       return true;
@@ -61,11 +103,22 @@ public class Item extends Block {
     }
   }
 
+  /**
+   * Sets the type and the amount to 1 of the item and changes it's image.
+   * @param type The type to set the item to.
+   */
+  
   public void set(String type) {
     this.type = type;
     this.amount = 1;
     changeImage(new Image("sprites/items/" + this.type + ".png"));
   }
+
+  /**
+   * Sets the type and the amount of the item and changes it's image.
+   * @param type The type to set the item to.
+   * @param amount The amount to set the item to.
+   */
 
   public void set(String type, int amount) {
     this.type = type;
